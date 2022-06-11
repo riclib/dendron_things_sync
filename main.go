b70503ab6269ef5a7ac71d29efd27667b7081553
Ricardo Liberato <ricardo@solidreason.com>
2022-06-11 23:02:51 +0100

    create items in things works

--- main.go
+++ main.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"errors"
-	"fmt"
 	"log"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark-meta"
-	"github.com/yuin/goldmark/parser"
-	gm "github.com/yuin/goldmark/text"
 	"gopkg.in/yaml.v2"
+	
 )
 
 type TaskData struct {
-	ID       string `yaml:"id"`
-	Title    string `yaml:"title"`
-	Desc     string `yaml:"desc"`
-	Updated  string `yaml:"updated"`
-	Created  string `yaml:"created"`
-	Status   string `yaml:"status"`
-	Due      string `yaml:"due"`
-	Priority string `yaml:"priority"`
-	Owner    string `yaml:"owner"`
+	ID       string  `yaml:"id"`
+	Title    string  `yaml:"title"`
+	Desc     string  `yaml:"desc"`
+	Updated  string  `yaml:"updated"`
+	Created  string  `yaml:"created"`
+	Status   *string `yaml:"status"`
+	Due      string  `yaml:"due"`
+	Priority string  `yaml:"priority"`
+	Owner    string  `yaml:"owner"`
 	Notes    string
+	Filepath string
 }
-
-
 
 const (
 	notesRoot   = "/Users/riclib/repo/notes/notes/task*.md"
@@ -41,18 +36,38 @@ const (
 )
 
 func main() {
-	
+	conn, err := sqlite.OpenConn(":memory:", sqlite.OpenReadWrite)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
 	matches, err := filepath.Glob(notesRoot)
 	if err != nil {
 		log.Fatal("Couldn't list files", err)
 	}
 	for _, match := range matches {
-		task, err := getTask(match)
+		task, wasTask, err := getTask(match)
+		task.Filepath = match
 		if err != nil {
 			log.Println("couldn't parse file", "file", match, "error", err)
 		}
-		fmt.Println(task)
+		if wasTask {
+			url, err := genThingsURL(task)
+			// fmt.Println(url)
+			if err != nil {
+				log.Println("couldn't generate things url", "err", err)
+			}
+			cmd := exec.Command("open", url)
+			// fmt.Println(cmd.String())
+			err = cmd.Run()
+			if err != nil {
+				panic(err)
+			}
+
+			// fmt.Println(url)
+			// time.Sleep(5 * time.Second)
+		}
 	}
 
 }
@@ -75,39 +90,42 @@ func EpochToTimeStr(date string) string {
 	return time.Unix(t/1000, 0).Format("2006-01-02")
 }
 
-func getTask( path string) (TaskData, error) {
+func getTask(path string) (t TaskData, wasTask bool, err error) {
 	var task TaskData
 
 	file, err := os.Open(path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
 	// Read and Check first Line
 	_, found := readUntilSeparator(scanner)
 	if !found {
-		return  task, errors.New("File doesn't start with ---")
+		return task, true, errors.New("File doesn't start with ---")
 	}
 
 	//Read Metadata
 	metadata, found := readUntilSeparator(scanner)
 	if !found {
-		return  task, errors.New("Couldn't find metadata")
+		return task, false, errors.New("Couldn't find metadata")
 	}
 	err = yaml.Unmarshal([]byte(metadata), &task)
 	if err != nil {
-		return  task, errors.New("Couldn't unmarshall task")
+		return task, false, errors.New("Couldn't unmarshall task")
+	}
+	if task.Status == nil { //not a task
+		return task, false, nil
 	}
 	err = parseDates(&task)
 	if err != nil {
-		return  task, errors.New("Couldn't parse dates")
+		return task, false, errors.New("Couldn't parse dates")
 	}
-
-	task.Notes = readUntil10k(scanner)
-	return task, nil
+	task.Notes = "dendron_id: " + task.ID + "\n" + "filepath: file:" + task.Filepath + "\n"
+	task.Notes = task.Notes + readUntilMaxSize(scanner, 10000-len(task.Notes))
+	return task, true, nil
 }
 
 // reads the file data until the next separator
@@ -124,11 +142,11 @@ func readUntilSeparator(scanner *bufio.Scanner) (string, bool) {
 	return "", false
 }
 
-func readUntil10k(scanner *bufio.Scanner) (string) {
+func readUntilMaxSize(scanner *bufio.Scanner, maxsize int) string {
 	text := ""
 	for scanner.Scan() {
 		line := scanner.Text()
-		if len(line) + len(text) > 10000 {
+		if len(line)+len(text) > maxsize {
 			break
 		}
 		text = text + line + "\n"
@@ -136,49 +154,22 @@ func readUntil10k(scanner *bufio.Scanner) (string) {
 	return text
 }
 
-
-func printTitles(markdown goldmark.Markdown, path string) error {
-
-	b, err := os.ReadFile(path)
-	mdreader := gm.NewReader(b)
-
-	if err != nil {
-		return err
-	}
-
-	context := parser.NewContext()
-	var buf bytes.Buffer
-	if err := markdown.Convert([]byte(b), &buf, parser.WithContext(context)); err != nil {
-		panic(err)
-	}
-
-	mdparser := parser.NewParser()
-	mdparser.Parse(mdreader, parser.WithContext(context))
-
-	metaData := meta.Get(context)
-	id := fmt.Sprint(metaData["id"])
-	title := fmt.Sprint(metaData["title"])
-	status, found := metaData["status"]
-	if found {
-		statusStr := fmt.Sprint(status)
-		fmt.Println(id + "[" + statusStr + "]: " + title)
-		url, err := genThingsURL(title, "No notes yet", time.Now(), "", time.Now(), time.Now(), false, false)
-		if err != nil {
-			log.Println("Error generating things url", err)
-		}
-		fmt.Println(url)
-	} else {
-		fmt.Println(id + ":" + title + " -> not a task")
-	}
-
-	return nil
-}
-
-func genThingsURL(title string, notes string, deadline time.Time, list string, creationDate time.Time, completionDate time.Time, completed bool, cancelled bool) (string, error) {
+func genThingsURL(task TaskData) (string, error) {
 	path := ""
-	path = addURLencoded(path, "title", title)
-	path = addURLencoded(path, "notes", notes)
+	path = addURLencoded(path, "tags", "dendron")
+	path = addURLencoded(path, "title", task.Title)
+	path = addURLencoded(path, "notes", task.Notes)
 	path = addURLencoded(path, "auth-token", thingsToken)
+	if *task.Status == "x" {
+		path = addURLencoded(path, "completed", "true")
+	}
+	if *task.Status == "-" {
+		path = addURLencoded(path, "canceled", "true")
+	}
+	path = addURLencoded(path, "creation-date", task.Created)
+	path = addURLencoded(path, "deadline", task.Due)
+	// Calback URLs were not reliable
+	//	path = addURLencoded(path, "x-success", "shortcuts://name=pingback&file="+task.Filepath)
 	return "things:///add?" + path, nil
 
 }
